b84221ab0720176b30e43a4fe81f99953443b005
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-11-27 12:05:40 +0100

    chore: use !important on static svelte logo

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
     $: button = logo ? 'Hide logo' : 'Show logo'
 </script>
 <main>
-  <span class="i-logos-svelte-icon w-6em h-6em transform transition-800 hover:rotate-180"></span>
+  <span class="i-logos-svelte-icon w-6em h-6em transform transition-800 !hover:rotate-180"></span>
 
   {#if logo}
     <span class:logo={logo} in:fly="{{ y: 200, duration: 2000 }}" out:fade></span>
